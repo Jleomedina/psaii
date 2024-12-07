4758cd1a5cf48cbee2a459115f43c7e6a5569bb0
Jleomedina <146792643+Jleomedina@users.noreply.github.com>
2024-12-06 21:24:06 -0600

    arreglo base de datos

--- configs/mariadb/user.sql
+++ configs/mariadb/user.sql
@@ -7,5 +7,5 @@ NOTA: Si su sistema operativo usa SElinux, debe
 configurarlo en modo 'permissive'.
 ==============================================================*/
 
-GRANT ALL PRIVILEGES ON *.* TO 'psa2'@'localhost' IDENTIFIED BY 'psa2';
+GRANT ALL PRIVILEGES ON *.* TO 'adminpsa'@'localhost' IDENTIFIED BY 'adminpsa';
 FLUSH PRIVILEGES;
